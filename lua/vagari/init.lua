7a5f3e5c321d9962ec00fef526e8b8ae319b4ce2
cullyn <cullyn.newman@gmail.com>
2026-03-29 21:08:37 -0700

    add dev hot-reload autocmd for faster iteration

--- lua/vagari/init.lua
+++ lua/vagari/init.lua
@@ -19,6 +19,24 @@ vagari.load = function()
 	require("vagari.languages").setup()
 	require("vagari.plugins").setup()
 	require("vagari.terminal").setup()
+
+	local plugin_dir = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":h:h:h")
+	if not vagari._dev_autocmd then
+		vagari._dev_autocmd = true
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			group = vim.api.nvim_create_augroup("VagariDevReload", { clear = true }),
+			pattern = plugin_dir .. "/*",
+			callback = function(ev)
+				for name, _ in pairs(package.loaded) do
+					if name:match("^vagari") then
+						package.loaded[name] = nil
+					end
+				end
+				require("vagari").load()
+				vim.notify("vagari: reloaded", vim.log.levels.INFO)
+			end,
+		})
+	end
 end
 
 return vagari
